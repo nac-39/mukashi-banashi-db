1cd59d915757ef4192dd27fcdf8addd7963fc3ac
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-06-10 20:56:49 +0900

    init

---
+++ addjson.py
@@ -0,0 +1,19 @@
+import json
+
+def main():
+    with open("./stories.json", "r",encoding="utf-8") as f:
+        question_json = json.loads(f.read())
+        while True:
+            new_title = input("title: ")
+            new_content = input("content: ")
+            if not(new_title) or not(new_content):
+                break
+            question_json.append({
+                "title": new_title,
+                "content": new_content
+            })
+    with open("./stories.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(question_json, indent=2, ensure_ascii=False))
+    
+if __name__ == "__main__":
+    main()
